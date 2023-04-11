6d36d37641a6aa724378e9de7f501a75e5685d9c
chs1825 <jhsnotnull@gmail.com>
2023-04-11 22:13:36 +0900

    ㄹ

--- src/main/java/com/memo/controller/TestController.java
+++ src/main/java/com/memo/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
 import java.util.List;
 
 @Controller
@@ -22,6 +23,19 @@ public class TestController {
 
     @RequestMapping("/")
     public String index(){
+
+        String path1 = "/Users/iirtech_009/devFiles/ksl/file/video/main/202304";
+        String path2 = "/Users/chs/devFiles/ksl/file/video/main/202304";
+        File file1 = new File(path1);
+        File file2 = new File(path2);
+
+        if(!file1.exists()){
+            file1.mkdirs();
+        }
+        if(!file2.exists()){
+            file2.mkdirs();
+        }
+
         return "index";
     }
 
