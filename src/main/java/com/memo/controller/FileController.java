3659efdaf4d92b550dd49a11891fc5aca987cbd9
chs1825 <jhsnotnull@gmail.com>
2023-04-13 18:00:33 +0900

    resolver 설정

--- src/main/java/com/memo/controller/FileController.java
+++ src/main/java/com/memo/controller/FileController.java
@@ -1,14 +1,16 @@
 package com.memo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
 @RequestMapping("/filePage")
+@Slf4j
 public class FileController {
+
 
     @RequestMapping("/start.do")
     public String makePage(){
@@ -20,9 +22,13 @@ public class FileController {
     public String fileUpload(@RequestParam MultipartFile file){
         
         String fileName = file.getOriginalFilename();
+
+        log.info("fileName : {} ," , fileName);
         System.out.println("fileName = " + fileName);
         
         String filePath = "a";
+
+
         
         
         
